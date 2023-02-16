063aecc4f43b5d5d822cf9d91ca91d036e5c20bc
saiemgilani <saiem.gilani@gmail.com>
2023-02-16 07:27:35 -0500

    Update ncaa_teams_info.R

--- R/ncaa_teams_info.R
+++ R/ncaa_teams_info.R
@@ -1,13 +1,3 @@
-
-## Update for following season by checking stats.ncaa.org/team/pages
-## Can probably just be updated in the csv manually
-ncaa_season_id_lu <- readr::read_csv("ncaa/ncaa_season_id_lu.csv")
-
-ncaa_season_id_lu <- ncaa_season_id_lu %>% 
-  baseballr:::make_baseballr_data("NCAA Baseball Season IDs from baseballr data repository", Sys.time())
-  
-readr::write_csv(ncaa_season_id_lu, "ncaa/ncaa_season_id_lu.csv")
-
 library(dplyr)
 library(purrr)
 library(baseballr)
